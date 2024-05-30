6173b8d32d397f5399c88c864f0e5f1c15bb14e6
ansadmk <ansadmk@gmail.com>
2024-05-30 11:21:20 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+ 
+    <v-sheet class="mx-auto" width="300">
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        :rules="rules"
+        label="First name"
+      ></v-text-field>
+      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+    </v-form>
+  </v-sheet>
+  
+
+ 
+</template>
+
+<style scoped>
+
+</style>
